6d42352eecb0d7991b01c5784350d92910270ce5
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-11-11 12:30:17 -0500

    temp

--- app.js
+++ app.js
@@ -5,43 +5,45 @@ const path = require('path')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
 
-
-
 mongoose
-  .connect('mongodb://127.0.0.1:27017/wolf-camp', { useNewUrlParser: true, useUnifiedTopology: true})
+  .connect('mongodb://127.0.0.1:27017/wolf-camp', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...🏁🏁🏁'))
   .catch((err) => console.error('Could not connect to MongoDB...🤬🤬🤬', err))
 
-
-
-
-
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('home')
 })
 
-
-app.get('/campgrounds', async(req, res) => {
+app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
 })
 
-app.get('/campgrounds/:id', async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
-
-  res.render('campgrounds/show', { campground })
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', { campground })
+})
+
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', { campground })
+})
 
 app.listen(3000, () => {
   l('listening on port 3000')
 })
-
