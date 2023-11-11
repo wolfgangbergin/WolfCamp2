dac62f025a4d158dc8c9e580c5a606d0caa8320e
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-11-11 06:57:09 -0500

    temp

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-  res.send('wolf')
+  res.render('home')
 })
 
 app.listen(3000, () => {
