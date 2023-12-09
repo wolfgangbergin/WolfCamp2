a8fa0bed07cb2e221834731c27d5d63cdc09ed8a
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-12-09 09:47:28 -0500

    temp

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
 const catchAsync = require('./utils/catchAsync')
-
-const validateCampground  = require('./utils/campgroundSchema')
-
+const Review = require('./models/review')
+const validateCampground = require('./utils/campgroundSchema')
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/wolf-camp', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect('mongodb://127.0.0.1:27017/wolf-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('Connected to MongoDB...🏁🏁🏁'))
   .catch((err) => console.error('Could not connect to MongoDB...🤬🤬🤬', err))
 
@@ -40,13 +42,10 @@ app.get(
 
 app.post(
   '/campgrounds',
-   validateCampground,
+  validateCampground,
   catchAsync(async (req, res, next) => {
-
-
-
     const campground = new Campground(req.body.campground)
-    
+
     await campground.save()
     res.redirect(`/campgrounds`)
   })
@@ -71,7 +70,11 @@ app.put(
   '/campgrounds/:id',
   validateCampground,
   catchAsync(async (req, res) => {
-    const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground }, { runValidators: true, new: true })
+    const campground = await Campground.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body.campground },
+      { runValidators: true, new: true }
+    )
     if (!campground) {
       throw new ExpressError('BAD Wolfie!!! 💩💩💩💩', 515)
     }
@@ -98,15 +101,29 @@ app.delete(
   })
 )
 
+app.post(
+  '/campgrounds/:id/reviews',
+  catchAsync(async (req, res) => {
+    l('kimbo')
+     const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    // l(review)
+    // campground.reviews.push(review)
+    // await campground.save()
+    // res.redirect(`/campgrounds/${req.params.id}`)
+
+    res.send('kimbo')
+  })
+)
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found🥜🥜', 404))
 })
 
 app.use((err, req, res, next) => {
- 
-if (!err.message) {err.message = 'Something went wrong',
-err.statusCode = 515}
+  if (!err.message) {
+    ;(err.message = 'Something went wrong'), (err.statusCode = 515)
+  }
   res.status(err.statusCode).render('error', { err })
 })
 
