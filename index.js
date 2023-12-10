71e7eeea701e1227fcc5a2296ea0b9bf2ca460d9
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-12-10 06:42:27 -0500

    temp

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('home')
