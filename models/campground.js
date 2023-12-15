4bfe42c8d5a88bf0b11db6ae2571ba11dd744018
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-12-15 09:13:12 -0500

    temp

--- models/campground.js
+++ models/campground.js
@@ -2,40 +2,28 @@ const mongoose = require('mongoose')
 const Campground = require('../models/campground')
 const Review = require('../models/review')
 
-
 const CampgroundSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    location: String,
-    wolfman: String,
-    reviews: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
-    })
-
+  title: String,
+  image: String,
+  price: Number,
+  description: String,
+  location: String,
+  wolfman: String,
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
+})
 
 CampgroundSchema.post('findOneAndDelete', async function (data) {
-  
-    if (data) {
-        const res = await Review.deleteMany({ _id: { $in: data.reviews } })
-        l(res)
-    }
-  
-}
-)
-
-// farmSchema.post('findOneAndDelete', async function (data) {
-//   if (data.products.length) {
-//     const res = await Product.deleteMany({ _id: { $in: data.products } })
-//     console.log(res)
-//   }
-// })
-
-
+  if (data) {
+   data.reviews.forEach(async(item)=>{
+   const temp1 = await Review.findByIdAndDelete(item._id)
+   l(temp1)
+   })
+  }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
