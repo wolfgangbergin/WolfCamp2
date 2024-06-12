5db502c67e4bd1188d9e90acc48399586361e555
Wolfgang Bergin <wolfgangbergin@gmail.com>
2024-06-12 08:45:56 -0400

    temp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,15 +1,4 @@
 
-
-const express = require('express')
-const app = express()
-const path = require('path')
-const mongoose = require('mongoose')
-const Campground = require('../models/campground')
-const methodOverride = require('method-override')
-const ejsMate = require('ejs-mate')
-const ExpressError = require('../utils/ExpressError')
-const catchAsync = require('../utils/catchAsync')
-const Review = require('../models/review')
 const router = express.Router();
 
 
--- wolfgang.js
+++ wolfgang.js
@@ -1,9 +1,23 @@
 globalThis.l = console.log
 globalThis.d = console.dir
 
-globalThis.wolfgang = {
-    
-}
+globalThis.express = require('express')
+globalThis.app = express()
+globalThis.path = require('path')
+globalThis.mongoose = require('mongoose')
+globalThis.Campground = require('./models/campground')
+globalThis.methodOverride = require('method-override')
+globalThis.ejsMate = require('ejs-mate')
+globalThis.ExpressError = require('./utils/ExpressError')
+globalThis.catchAsync = require('./utils/catchAsync')
+globalThis.Review = require('./models/review')
+globalThis.campgrounds = require('./routes/campgrounds')
+globalThis.reviews = require('./routes/reviews')
+globalThis.wolf = require('./routes/wolf')
+//globalThis.router = express.Router()
 
+globalThis.wolfgang = {
+    routerr: express.Router(),
+}
 
 exports
