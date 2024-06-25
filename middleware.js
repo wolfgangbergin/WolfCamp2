e53db78a03959d952810ad090d3ef23cd136c594
Wolfgang Bergin <wolfgangbergin@gmail.com>
2024-06-25 13:25:55 -0400

    working

--- middleware.js
+++
@@ -1,9 +0,0 @@
-const isLoggedIn = (req, res, next) => {
-if (!req.isAuthenticated()) {
-    req.flash('error', 'You must be signed in')
-    return res.redirect('/wolfman/login')
-  }
-    next()
-}
-
-module.exports = isLoggedIn
