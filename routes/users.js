fa5e207b913d2651ed79aa8d9e751e4fbc2862a3
Wolfgang Bergin <wolfgangbergin@gmail.com>
2024-07-05 15:47:57 -0400

    userRoutesWorking

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,20 @@
-router.get('/register', usersController.user_register)
+router
+  .route('/register')
+  .get(usersController.user_register)
+  .post(usersController.user_register_post)
 
-router.post('/register', usersController.user_register_post)
+router
+  .route('/login')
 
-router.get('/login', usersController.user_login)
+  .get(usersController.user_login)
 
-router.post(
-  '/login',
-
-  passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/user/login',
-  }),
-  usersController.user_login_post
-)
+  .post(
+    passport.authenticate('local', {
+      failureFlash: true,
+      failureRedirect: '/user/login',
+    }),
+    usersController.user_login_post
+  )
 
 router.get('/logout', usersController.user_logout)
 
