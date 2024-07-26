be4c3fedba80e0f04bd19f60ab1a2e78afea57d9
Wolfgang Bergin <wolfgangbergin@gmail.com>
2024-07-26 16:16:40 -0400

    temp

--- test.js
+++ test.js
@@ -12,16 +12,3 @@
   </div>
 </div>
 </div>
-
-
-
-
-<!-- <div class="mb-3">
-                  <label class="form-label" for="image">Image</label>
-                  <input required class="form-control" type="text" name="campground[image]" id="image" value="https://www.desmoinesregister.com/gcdn/-mm-/a3bf82da6e654f6ea9c4df986dbf0437dc9f9c72/c=0-125-2762-1685/local/-/media/2015/02/23/IAGroup/DesMoines/635603064337329282-des.M0223FDFire009.JPG?width=700&height=396&fit=crop&format=pjpg&auto=webp"></input>
-                  <div class="invalid-feedback">
-                    Please provide a valid image.
-              </div>
-                </div> -->
-
-                
